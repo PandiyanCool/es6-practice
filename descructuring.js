13360693fdba7774edadfb5eca826a643426a626
Pandiyan <pandiyanit7@gmail.com>
2017-04-21 10:02:22 +0530

    added destruct

--- descructuring.js
+++ descructuring.js
@@ -4,16 +4,78 @@ var one, two, three;
 
 console.log(`${one} ${two}`);
 
-
-
 var fruits = [
     'mango',
     'banana',
     'jack fruit'
 ];
 
-var [fruitOne, fruitTwo] = fruits;
+var [fruitOne, fruitTwo, fruitThree] = fruits;
 
 console.log(`${fruits}`);
 
-console.log(`${fruitOne} ${fruitTwo}`);
+console.log(`${fruitOne} ${fruitTwo} ${fruitThree}`);
+
+// Object Desctructuring
+// ES5
+var username = {
+    firstName: 'Pandiyan',
+    lastName: 'Murugan'
+};
+
+var firstName = username.firstName;
+var lastName = username.lastName;
+
+console.log(firstName + ' ' + lastName);
+
+
+// ES6
+
+var username = {
+    firstName: 'MS',
+    lastName: 'Dhoni'
+};
+
+var { firstName, lastName } = username;
+
+console.log(firstName + ' ' + lastName);
+
+
+// syntax error
+
+// var { firstName, lastName };
+
+
+// default values
+
+var username = {
+    firstName: 'MS',
+    lastName: 'Dhoni'
+};
+
+var { firstName, lastName, designation } = username;
+
+console.log(firstName + ' ' + lastName + ' ' + designation);
+// designation will be undefined
+
+var username = {
+    firstName: 'MS',
+    lastName: 'Dhoni'
+};
+
+var { firstName, lastName, designation = 'player' } = username;
+
+console.log(firstName + ' ' + lastName + ' ' + designation);
+
+// designation will be set as player if it didn't find the value for player in username object
+
+
+
+var username = {
+    firstName: 'MS',
+    lastName: 'Dhoni'
+};
+
+var {  lastName: localLastName, firstName: localFirstName } = username;
+
+console.log(localFirstName +' '+ localLastName);     // MS Dhoni
